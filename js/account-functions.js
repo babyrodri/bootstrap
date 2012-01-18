5e1705ee9fd30509c40a33ecedb4df43073ec0e6
Bryan Clark <clarkbw@gmail.com>
2012-01-18 14:55:14 -0800

    created a widgets page that holds all the elements we are using in each of the details and index page

---
+++ js/account-functions.js
@@ -0,0 +1,35 @@
+// account-functions.js
+function login(email) {
+  $.cookie('email', email);
+  var gravatar = "http://www.gravatar.com/avatar/" + $.md5(email) + "?s=16px";
+  $.cookie('gravatar', gravatar);
+  $("#signin-form").hide();
+  $("#signedin").show().find(".email").text(email);
+  $("#signedin").find(".avatar").attr("src", gravatar);
+}
+
+function logout() {
+  $.cookie('email', null);
+  $.cookie('gravatar', null);
+  $("#signin-form").show();
+  $("#signedin").hide();
+}
+
+$(document).ready(function () {
+
+  if ($.cookie('email')) {
+    login($.cookie('email'));
+  }
+
+  $("#signin").click(function () {
+    var val = $(this).prev("input[type=text]").val();
+    if (val) {
+      login(val);
+    }
+    return false;
+  });
+
+  $("#logout").click(function () {
+    logout();
+  });
+});
