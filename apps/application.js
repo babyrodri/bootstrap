6b54bbb661dec0c70c03e4376de8e9ec5a93c616
Bryan Clark <clarkbw@gmail.com>
2012-02-02 10:48:45 -0800

    get a working purchase button for the details page

--- apps/application.js
+++ apps/application.js
@@ -1,5 +1,22 @@
 $(document).ready(function () {
+
   $("a.more").click(function () {
     $(this).parents(".app-description").find("p.more").slideToggle();
   });
+
+  $("#purchase").click(function () {
+    var btn = $(this);
+    if ($(this).data("purchased")) {
+        btn.button("installing");
+        window.setTimeout(function () {
+          btn.attr("disabled", true).text("installed");
+        }, 3000);
+    } else {
+        btn.button("purchasing");
+        window.setTimeout(function () {
+          btn.data("purchased", true).button('purchased');
+        }, 3000);
+    }
+  });
+
 });
